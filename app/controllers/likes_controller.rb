a84e3248b39293216b7bf140bf149087ea9d409e
00komiya <114364060+00komiya@users.noreply.github.com>
2022-12-13 05:37:12 +0000

    [add]いいね非同期

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
     @item = Item.find(params[:item_id])
     like = current_user.likes.new(item_id: @item.id)
     like.save
-    redirect_to request.referer
+    # redirect_to request.referer 非同期処理
   end
 
   def destroy
     @item = Item.find(params[:item_id])
     like = current_user.likes.find_by(item_id: @item.id)
     like.destroy
-    redirect_to request.referer
+    # redirect_to request.referer 非同期処理
   end
 end
