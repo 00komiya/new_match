5767a40470eaee3bdedeca28da569d0b007b36c8
00komiya <114364060+00komiya@users.noreply.github.com>
2022-12-13 13:52:41 +0000

    [add]キーワード検索途中

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,25 @@
 class SearchesController < ApplicationController
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+  def search_for(model, content, method)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      else
+        User.where('name LIKE ?', '%'+content+'%')
+      end
+    elsif model == 'item'
+      if method == 'perfect'
+        Item.where(title: content)
+      else
+        Item.where('title LIKE ?', '%'+content+'%')
+      end
+    end
+  end
 end
