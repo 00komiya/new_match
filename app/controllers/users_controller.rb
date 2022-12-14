879ff891ae420d4f70d4fb8f8ef837a72087a170
00komiya <114364060+00komiya@users.noreply.github.com>
2022-12-14 13:56:31 +0000

    [add]ユーザーページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ class UsersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit]
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @items = @user.items
   end
 
   def edit
