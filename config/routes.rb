21fa5c084466721341cc28f3033cd96f51225db2
00komiya <114364060+00komiya@users.noreply.github.com>
2022-12-18 02:22:04 +0000

    [add]管理者ｕｒｌ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
   get "search" => "searches#search"
-
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
-  end
 
   resources :users
 
@@ -15,4 +10,21 @@ Rails.application.routes.draw do
    resources :item_comments, only: [:create, :destroy]
    resources :item_tags, only: [:destroy]
   end
+
+  namespace :admin do
+    resources :items, only: [:index, :show, :destroy]
+    resources :item_comments, only: [:destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+    # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip:[:registrations, :passwords],controllers:{
+    sessions: "admin/sessions"
+  }
+    #ゲストログイン
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_15_140544) do
+ActiveRecord::Schema.define(version: 2022_12_18_010909) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,18 @@ ActiveRecord::Schema.define(version: 2022_12_15_140544) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "item_comments", force: :cascade do |t|
@@ -88,6 +100,7 @@ ActiveRecord::Schema.define(version: 2022_12_15_140544) do
     t.string "age"
     t.string "sex"
     t.string "address"
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
