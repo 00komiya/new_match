002525e1340f57e17bbb7f032ef0c47e4afad6a8
00komiya <114364060+00komiya@users.noreply.github.com>
2022-12-22 13:51:14 +0000

    [add]seeds一部登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,53 @@ Admin.create(
   email: "admin@admin",
   password: "adminadmin"
   )
+
+users = User.create!(
+  [
+    {
+      email: 'tanka@test.com',
+      name: '田中',
+      password:'tanakatanaka',
+      introduction: 'よろしくお願いします',
+      age: '20',
+      sex: '男',
+      address: '東京',
+      profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("app/assets/images/pasta.jpg"), filename:"open.jpg")},
+    {
+      email: 'hanako@test.com',
+      name: 'はなこ',
+      password:'hanakohanako',
+      introduction: 'スイーツ大好き！絡んでください',}
+  ]
+)
+
+items = Item.create(
+  [
+    {
+      name: 'カルボナーラ',
+      shop_name: 'セブンイレブン',
+      introduction: '温玉が二つになって新登場!!',
+      user_id: users[0].id ,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("app/assets/images/pasta.jpg"), filename: 'default-image.jpg')},
+    {
+      name: 'マリトッツォ',
+      shop_name: 'ローソン',
+      introduction: 'コラボ品！食べた人感想教えてください！',
+       user_id: users[1].id ,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("app/assets/images/sweets.jpg"), filename: 'default-image.jpg')}
+  ]
+)
+
+  Tag.create(name: "お弁当")
+  Tag.create(name: "おにぎり")
+  Tag.create(name: "パン")
+  Tag.create(name: "パスタ")
+  Tag.create(name: "麺類")
+  Tag.create(name: "ホットスナック")
+  Tag.create(name: "スイーツ")
+  Tag.create(name: "アイス")
+  Tag.create(name: "お菓子")
+  Tag.create(name: "ドリンク")
+  Tag.create(name: "アルコール")
+  Tag.create(name: "コラボ")
+  Tag.create(name: "限定品")
