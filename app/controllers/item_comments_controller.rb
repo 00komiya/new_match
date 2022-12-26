619d8ae17550f8409b03921a48e544a29e1f97a5
00komiya <114364060+00komiya@users.noreply.github.com>
2022-12-26 11:08:43 +0000

    [fix]コメントの表示

--- app/controllers/item_comments_controller.rb
+++ app/controllers/item_comments_controller.rb
@@ -1,6 +1,7 @@
 class ItemCommentsController < ApplicationController
   def create
-    @item_comments = ItemComment.eager_load(:user).where('users.is_deleted = ?', false)
+    @item = Item.find(params[:item_id])
+    @item_comments = @item.item_comments.eager_load(:user).where('users.is_deleted = ?', false)
     @comment = current_user.item_comments.new(item_comment_params)
     @comment.item_id = params[:item_id]
     @comment.save
@@ -12,7 +13,8 @@ class ItemCommentsController < ApplicationController
 
   def destroy
     ItemComment.find_by(id: params[:id], item_id: params[:item_id]).destroy
-    @item_comments = ItemComment.eager_load(:user).where('users.is_deleted = ?', false)
+    @item = Item.find(params[:item_id])
+    @item_comments = @item.item_comments.eager_load(:user).where('users.is_deleted = ?', false)
     @item_comment = ItemComment.new(item_id: params[:item_id])
     # redirect_to request.referer 非同期処理
   end
