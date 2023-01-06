b3cf568176c206a64aa8cbd62fc32f23b3a14663
00komiya <114364060+00komiya@users.noreply.github.com>
2023-01-06 12:04:08 +0000

    [add]ページトップ

---
+++ app/javascript/main.js
@@ -0,0 +1,17 @@
+jQuery(function() {
+    var pagetop = $('#page_top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+});
