33cdbbc50222744139a1fe4e6d2661e897a06e58
00komiya <114364060+00komiya@users.noreply.github.com>
2023-01-24 12:25:25 +0000

    [update]アクション権限

--- app/controllers/item_tags_controller.rb
+++ app/controllers/item_tags_controller.rb
@@ -1,23 +1,19 @@
 class ItemTagsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :ensure_correct_user, only: [:destroy]
+  before_action :ensure_correct_user, only: [:destroy]
 
   def destroy
     @item = Item.find(params[:item_id])
     @item_tag = ItemTag.find(params[:id])
-    # if @item_tag.user == current_user
-      @item_tag.delete
-      redirect_to item_path(@item)
-    # else
-    #   redirect_to items_path
-    # end
+    @item_tag.delete
+    redirect_to item_path(@item)
   end
 
-  # def ensure_correct_user
-  #   @item_tag = ItemTag.find(params[:id])
-  #   unless @item_tag.user == current_user
-  #     redirect_to items_path
-  #   end
-  # end
+  def ensure_correct_user
+    @item_tag = ItemTag.find(params[:id])
+    unless @item_tag.item.user == current_user
+      redirect_to items_path
+    end
+  end
 
 end
