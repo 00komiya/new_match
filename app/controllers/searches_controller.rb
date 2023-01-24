11baf58d994fe05737cf53a7112db33b7fc766f0
00komiya <114364060+00komiya@users.noreply.github.com>
2023-01-24 11:24:20 +0000

    [update]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,11 +6,11 @@ class SearchesController < ApplicationController
     @content = params[:content]
     @method = params[:method]
     if @model == 'item'
-      @records = Item.search_for(@content, @method).page(params[:page])
+      @records = Item.search_for(@content, @method).order(created_at: "DESC").page(params[:page])
     elsif @model == 'user'
-      @records = User.search_for(@content, @method).page(params[:page]).per(10)
+      @records = User.search_for(@content, @method).order(created_at: "DESC").page(params[:page]).per(10)
     elsif @model == 'tag'
-      @records = Item.left_joins(:tags).where(tags: {id: @content}).page(params[:page])
+      @records = Item.left_joins(:tags).where(tags: {id: @content}).order(created_at: "DESC").page(params[:page])
       @content = Tag.find(@content).name
     end
   end
